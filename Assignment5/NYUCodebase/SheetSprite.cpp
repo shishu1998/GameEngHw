8273a7910d775b25760060465f3c5c2467472564
shishu1998 <shishu_1998@yahoo.com>
2018-04-03 19:15:02 -0400

    backward compatibility

--- Assignment5/NYUCodebase/SheetSprite.cpp
+++ Assignment5/NYUCodebase/SheetSprite.cpp
@@ -17,12 +17,12 @@ void SheetSprite::Draw(ShaderProgram * program)
 	};
 	float aspect = width / height;
 	float vertices[] = {
-		-0.5f * size * aspect, -0.5f * size,
-		0.5f * size * aspect, 0.5f * size,
-		-0.5f * size * aspect, 0.5f * size,
-		0.5f * size * aspect, 0.5f * size,
-		-0.5f * size * aspect, -0.5f * size ,
-		0.5f * size * aspect, -0.5f * size };
+		-0.5f * aspect, -0.5f,
+		0.5f * aspect, 0.5f,
+		-0.5f * aspect, 0.5f,
+		0.5f * aspect, 0.5f,
+		-0.5f * aspect, -0.5f,
+		0.5f * aspect, -0.5f};
 
 	glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
 	glEnableVertexAttribArray(program->positionAttribute);
